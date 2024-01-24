fff786803540adcc9c205cd6f94af9442a293027
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-01-24 21:05:06 +0530

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,14 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="grid grid-cols-12 gap-6">
-        <div className="hidden md:col-span-2">
+        <div className="hidden md:col-span-2 md:block">
           <LeftSide />
         </div>
         <div className="col-span-12 max-h-fit rounded-md border bg-background md:col-span-8">
           <PlateEditor />
         </div>
 
-        <div className="hidden md:col-span-2">
-          <LeftSide />
-        </div>
+        <div className="hidden md:col-span-2 md:block"></div>
       </div>
     </section>
   );
